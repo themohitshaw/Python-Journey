51876e2722c9bc09ad763ddf75d3617e9ae72154
Mohit Kumar Shaw <mohitshaw188@gmail.com>
2026-05-31 21:14:13 +0530

    Practice Set-1
    This set is based on the topics we’ve covered so far: installation, syntax, variables,
    typecasting, user input, comments, and operators.

---
+++ practice_set_1/04_question4.py
@@ -0,0 +1,8 @@
+num = "45"
+
+num_int = int(num)
+
+num_int+=10
+
+print(num_int)
+
---
+++ practice_set_1/06_question6.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+
+print(f"Addition: ",{num1+num2}, f"Substraction: ",{num1-num2}, f"Multiplication: ",{num1*num2}, f"Division: ",{num1/num2},)
