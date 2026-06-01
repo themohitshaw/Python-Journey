3d03e7effce17fc285804a8f42709584f0ccb4ca
Mohit Kumar Shaw <mohitshaw188@gmail.com>
2026-06-01 11:50:18 +0530

    Loops and break , continue , pass concept
    for
    while
    break
    continue
    pass

---
+++ 09_loops/01_for_loops.py
@@ -0,0 +1,11 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# for i in range(1, 6): # range function goes from 1 to (6-1) ie 5 in this case
+#     print(i)
+
+for i in range(1, 11):
+    print("5 X", i, "=",  5*i)
---
+++ 09_loops/02_while_loop.py
@@ -0,0 +1,10 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+i = 1
+while i<6:
+    print(i)
+    i = i + 1
