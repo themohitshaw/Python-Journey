88b2d8ed42d9812dd5260c8b0d37120a546c9b03
Mohit Kumar Shaw <mohitshaw188@gmail.com>
2026-06-01 11:48:27 +0530

    Add files via upload

---
+++ 09_loops/05_continue.py
@@ -0,0 +1,6 @@
+for i in range(1, 20):
+    if i == 10:
+        continue # continue the loop for the next iteration here itself
+    print(i)
+
+    
---
+++ 09_loops/06_pass.py
@@ -0,0 +1,5 @@
+i = 3
+
+if i == 32:
+    pass # do nothing
+print("End of program")
